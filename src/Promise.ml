42f199a5ca01464a353acc40d1c6a5bec7b33d9f
Risto Stevcev <me@risto.codes>
2020-03-15 13:12:25 +0100

    Added odoc generation and some code cleanup

--- src/Promise.ml
+++ src/Promise.ml
@@ -1,13 +1,13 @@
 open LambdaStreams
 
-let first_to_promise (stream : 'a Finite.Async.t) : 'a Js.Promise.t =
+let first_to_promise stream =
   Js.Promise.make (fun ~resolve ~reject:_ ->
       stream
       |> Async.listen (function
              | Signal.Data value -> ( resolve value [@bs])
              | _ -> ()))
 
-let last_to_promise (stream : 'a Finite.Async.t) : 'a Js.Promise.t =
+let last_to_promise stream =
   let latest_value = ref None in
   Js.Promise.make (fun ~resolve ~reject:_ ->
       stream
@@ -17,7 +17,7 @@ let last_to_promise (stream : 'a Finite.Async.t) : 'a Js.Promise.t =
              | EndOfSignal, Some value -> ( resolve value [@bs])
              | _ -> ()))
 
-let from_promise (promise : 'a Js.Promise.t) : 'a Finite.Async.t =
+let from_promise promise =
   Async.make @@ fun cb ->
   promise
   |> Js.Promise.then_ (fun value ->
--- src/Promise.mli
+++ src/Promise.mli
@@ -5,4 +5,3 @@ val first_to_promise : 'a Finite.Async.t -> 'a Js.Promise.t
 val last_to_promise : 'a Finite.Async.t -> 'a Js.Promise.t
 
 val from_promise : 'a Js.Promise.t -> 'a Finite.Async.t
-
